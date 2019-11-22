41a7fdc44ef8d0c01b548fa765e87e9c59e2d801
Zack Chestler <zack.chestler@nwea.org>
2019-11-21 19:44:30 -0800

    Add cardEffect to header file so tests can use it and compile

--- projects/FinalProject-Bugs/dominion/dominion.h
+++ projects/FinalProject-Bugs/dominion/dominion.h
@@ -128,4 +128,6 @@ int getWinners(int players[MAX_PLAYERS], struct gameState *state);
 /* Set array position of each player who won (remember ties!) to
    1, others to 0 */
 
+int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState *state, int handPos, int *bonus);
+
 #endif
