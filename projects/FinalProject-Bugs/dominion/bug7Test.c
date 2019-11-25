bdcb0280f8cfcf2e2713d0c39f4e385cb9aff3bd
Zack Chestler <zack.chestler@nwea.org>
2019-11-24 16:17:38 -0800

    Add test for bug 7

--- projects/FinalProject-Bugs/dominion/bug7Test.c
+++ projects/FinalProject-Bugs/dominion/bug7Test.c
@@ -24,41 +24,56 @@ void bugTest() {
 
 	initializeGame(numPlayers, k, seed, &state);
 
-    printf("\nTesting Bug ####\n\n");
-    printf("_____Test name here\n\n");
+    printf("\nTesting Bug 07\n\n");
+    printf("_____player gains 4 coins when the revealed cards are a copper and a gold and gains no other benefits\n\n");
 
 	// get current player
 	int currentPlayer = whoseTurn(&state);
+	int opposingPlayer = 1;
 	int handPos = 4;// this is where the test card will go
+
+	state.discardCount[opposingPlayer] = 0;
+	state.deckCount[opposingPlayer] = 2;
+
+	state.deck[opposingPlayer][0] = copper;
+	state.deck[opposingPlayer][1] = gold;
 
 	// Fill current players hand up with coppers
 	for (i = 0; i < 5; i++) {
 		state.hand[currentPlayer][i] = copper;
 	}
 	// add/edit other cards to hand for testing
-	state.hand[currentPlayer][handPos] = mine; // add test card to hand to play
+	state.hand[currentPlayer][handPos] = tribute; // add test card to hand to play
 
     memcpy(&testState, &state, sizeof(struct gameState));
 
 	// params for test
-	int card = mine;
-    int choice1 = 0; // there is a copper at hand index 0
-    int choice2 = silver;
-    int choice3 = 0; // not used for mine
-    int bonus = 0; // not used for mine
+	int card = tribute;
+    int choice1 = 0; // not used for tribute
+    int choice2 = 0; // not used for tribute
+    int choice3 = 0; // not used for tribute
+    int bonus = 0; // not used for tribute
 
     cardEffect(card, choice1, choice2, choice3, &testState, handPos, &bonus);
 
 	// played card count should be equal to state.playedCardCount + 1
 	// The only card that will be in played card count is the actual mine card
-    int fail = assert(state.playedCardCount + 1 , testState.playedCardCount);
+	int fail = assert(testState.coins, state.coins + 4);
 
     if(fail) {
-        printf("Failed - <Enter failed message>\n");
+        printf("Failed - player should gain 4 coins\n");
     }
     else {
-        printf("Passed - <enter passed message>\n");
+        printf("Passed - player gains 4 coins\n");
     }
+    fail = assert(testState.numActions, state.numActions);
+
+    if(fail) {
+            printf("Failed - player should not have gained any actions\n");
+        }
+        else {
+            printf("Passed - player did not gain any actions\n");
+        }
 }
 
 int main(int argc, char *argv[])
