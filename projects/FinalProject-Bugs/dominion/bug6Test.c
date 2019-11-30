9789017531d482b73f234aa41c3eaea57d1f27d3
Steven <lords@oregonstate.edu>
2019-11-29 19:44:43 -0800

    Add unit test for Bug #6

--- projects/FinalProject-Bugs/dominion/bug6Test.c
+++ projects/FinalProject-Bugs/dominion/bug6Test.c
@@ -15,17 +15,17 @@ int assert(int got, int want) {
 }
 
 void bugTest() {
-    int i, choice1, choice2, choice3, handPos, bonus, card, returnVal, fail;
+    int i, choice1, choice2, choice3, handPos, bonus, card, fail;
     int seed = 1000;
     int numPlayers = 2;
     struct gameState state, testState;
-    int k[10] = {baron, remodel, gardens, minion, mine, steward,
+    int k[10] = {baron, feast, gardens, minion, mine, steward,
             minion, tribute, ambassador, council_room};
 
     initializeGame(numPlayers, k, seed, &state);
 
     printf("\nTesting Bug #03\n\n");
-    printf("Remodel case returns -1 for valid choice1 and choice2 inputs\n\n");
+    printf("Feast case allows gaining a card greater than 5\n\n");
 
     state.whoseTurn = 0;
 
@@ -33,6 +33,7 @@ void bugTest() {
     for (i = 0; i < 5; i++) {
         state.hand[state.whoseTurn][i] = copper;
     }
+    state.handCount[state.whoseTurn] = 5;
 
     state.discard[0][state.discardCount[0]] = copper;
     state.discardCount[0]++;
@@ -47,9 +48,13 @@ void bugTest() {
     bonus = 0; 
     handPos = 0;
 
-    testState.hand[testState.whoseTurn][handPos] = feast; // add Remodel to hand
+    testState.hand[testState.whoseTurn][0] = feast; // add Feast to hand
 
-    returnVal = cardEffect(card, choice1, choice2, choice3, &testState, handPos, &bonus);
+    testState.hand[testState.whoseTurn][1] = gold;
+
+    testState.supplyCount[province] = 4;
+
+    cardEffect(card, choice1, choice2, choice3, &testState, handPos, &bonus);
 
     //Test that the return value of cardEffect is what we expect
     fail = assert(testState.discard[0][testState.discardCount[0]-1], state.discard[0][state.discardCount[0]-1]);
