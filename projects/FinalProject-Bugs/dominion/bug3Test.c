92aff9f6ce802d35c78912b3d268b0f8bf1e009b
Steven <lords@oregonstate.edu>
2019-11-29 16:35:21 -0800

    Add unit test for bug 3

--- projects/FinalProject-Bugs/dominion/bug3Test.c
+++ projects/FinalProject-Bugs/dominion/bug3Test.c
@@ -15,54 +15,101 @@ int assert(int got, int want) {
 }
 
 void bugTest() {
-    int i;
+    int i, choice1, choice2, choice3, handPos, bonus, card, returnVal, fail;
 	int seed = 1000;
 	int numPlayers = 2;
 	struct gameState state, testState;
-	int k[10] = {baron, feast, gardens, minion, mine, steward,
-			sea_hag, tribute, ambassador, council_room};
+	int k[10] = {baron, remodel, gardens, minion, mine, steward,
+			minion, tribute, ambassador, council_room};
 
 	initializeGame(numPlayers, k, seed, &state);
 
-    printf("\nTesting Bug ####\n\n");
-    printf("_____Test name here\n\n");
+    printf("\nTesting Bug #03\n\n");
+    printf("Remodel case returns -1 for valid choice1 and choice2 inputs\n\n");
 
-	// get current player
-	int currentPlayer = whoseTurn(&state);
-	int handPos = 4;// this is where the test card will go
+    state.whoseTurn = 0;
 
 	// Fill current players hand up with coppers
 	for (i = 0; i < 5; i++) {
-		state.hand[currentPlayer][i] = copper;
+		state.hand[state.whoseTurn][i] = copper;
 	}
-	// add/edit other cards to hand for testing
-	state.hand[currentPlayer][handPos] = mine; // add test card to hand to play
+
+
+    /******************************************************************************
+     * Positive Test Case
+    ******************************************************************************/
 
     memcpy(&testState, &state, sizeof(struct gameState));
 
 	// params for test
-	int card = mine;
-    int choice1 = 0; // there is a copper at hand index 0
-    int choice2 = silver;
-    int choice3 = 0; // not used for mine
-    int bonus = 0; // not used for mine
+	card = remodel;
+    choice1 = 1; 
+    choice2 = minion;
+    choice3 = 0;
+    bonus = 0; 
+    handPos = 0;
 
-    cardEffect(card, choice1, choice2, choice3, &testState, handPos, &bonus);
+	testState.hand[testState.whoseTurn][handPos] = remodel; // add Remodel to hand
+	testState.hand[testState.whoseTurn][choice1] = gardens; // add choice1 card to hand
 
-	// played card count should be equal to state.playedCardCount + 1
-	// The only card that will be in played card count is the actual mine card
-    int fail = assert(state.playedCardCount + 1 , testState.playedCardCount);
+    returnVal = cardEffect(card, choice1, choice2, choice3, &testState, handPos, &bonus);
+
+    //Test that the return value of cardEffect is what we expect
+    fail = assert(returnVal, 0);
 
     if(fail) {
-        printf("Failed - <Enter failed message>\n");
+        printf("Failed - return value is -1, expected is 0\n");
     }
     else {
-        printf("Passed - <enter passed message>\n");
+        printf("Passed - return value is 0, expected is 0\n");
+    }
+
+
+
+
+
+    /******************************************************************************
+     * Negative Test Case
+    ******************************************************************************/
+
+    memcpy(&testState, &state, sizeof(struct gameState));
+
+	// params for test
+	card = remodel;
+    choice1 = 1; 
+    choice2 = gold;
+    choice3 = 0;
+    bonus = 0; 
+    handPos = 0;
+
+	testState.hand[testState.whoseTurn][handPos] = remodel; // add Remodel to hand
+	testState.hand[testState.whoseTurn][choice1] = silver; // add choice1 card to hand
+
+    returnVal = cardEffect(card, choice1, choice2, choice3, &testState, handPos, &bonus);
+
+    //Test that the return value of cardEffect is what we expect
+    fail = assert(returnVal, -1);
+
+    if(fail) {
+        printf("Failed - return value is 0, expected is -1\n");
+    }
+    else {
+        printf("Passed - return value is -1, expected is -1\n");
     }
 }
+
+
+
+
+/******************************************************************************
+ * Main
+******************************************************************************/
+
 
 int main(int argc, char *argv[])
 {
     bugTest();
     return 0;
 }
+
+
